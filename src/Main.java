10c57a45c45843e1065855cf0dd431c2f63a11c4
Amanda de Oliveira Pinheiro <amandajauregui.dev@gmail.com>
2024-04-19 20:55:28 -0300

    metodos string

--- src/Main.java
+++ src/Main.java
@@ -1,6 +1,9 @@
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello and welcome!");
+        String var1 = "Olá, sou amandinha.";
+        double numero1 = 13.5;
+        String varnum = String.valueOf(numero1);
+        System.out.println(varnum.charAt(0));
     }
 }
 
